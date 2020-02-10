dc89aa0b9ee45edc54647590514c13d4dbc6bb86
osman kocak <osmankocak212@gmail.com>
2020-02-10 10:41:46 +0200

    Update SIT-BETA1.2.py

--- SIT-BETA1.2.py
+++ SIT-BETA1.2.py
@@ -4,10 +4,11 @@
 import os
 
 os.system("apt-get install figlet")
+os.system("apt-get install nikto")
 os.system("clear")
 os.system("figlet Sytem Info Tool")
 
-print("""
+M = ("""
 Port Tarama Programı
 
 1) Port Tarama Islemleri
@@ -16,19 +17,50 @@ Port Tarama Programı
 4) Guvenlik Duvari Testeri (Waffw00f)
 
 
+
 """)
 
-islem = input("Yapmak Istediginiz Islem Numarasini Giriniz:")
+print(M)
 
-if(islem == 1):
-    ip = input("Hedef IP Giriniz: ")
-    os.system("nmap -sS -sV " + ip)
-elif (islem == 2):
-    ip = input("Hedef IP Giriniz: ")
+islem = raw_input("Yapmak Istediginiz Islem Numarasini Giriniz:")
+
+if(islem == "1"):
+    ip = raw_input("Hedef IP Giriniz: ")
+    os.system("clear")
+    os.system("nmap -sS -sV  "+ ip)
+    q=raw_input("----------- ANA MENUYE GITMEK ISTIYORSANIZ (Y) BASIN -----------: " )
+    if(q == "Y"):
+	os.system("clear")
+	os.system("figlet Sytem Info Tool")
+	print(M)
+
+    
+elif (islem == "2"):
+    ip = raw_input("Hedef IP Giriniz: ")
     os.system("nikto -h " + ip)
-elif (islem == 3):
+    q=raw_input("----------- ANA MENUYE GITMEK ISTIYORSANIZ (Y) BASIN -----------: " )
+    if(q == "Y"):
+	os.system("clear")
+	os.system("figlet Sytem Info Tool")
+	print(M)
+
+elif (islem == "3"):
     ip = raw_input("Hedef IP veya URL Giriniz: ")
-    os.system("wpscan --ignore-main-redirect  --enumerate u --enumerate vt --enumerate vp --url" + ip)
-elif (islem == 4):
-    ip = raw_input()"Hedef URL Giriniz (Ornek: www.ornek.com): ")
+    os.system("wpscan --url "+ ip +" --ignore-main-redirect  --enumerate u --enumerate vt --enumerate vp ")
+    q=raw_input("----------- ANA MENUYE GITMEK ISTIYORSANIZ (Y) BASIN -----------: " )
+    if(q == "Y"):
+	os.system("clear")
+	os.system("figlet Sytem Info Tool")
+	print(M)
+    
+elif (islem == "4"):
+    ip = raw_input("Hedef URL Giriniz (Ornek: www.ornek.com): ")
     os.system("wafw00f " + ip)
+    q=raw_input("----------- ANA MENUYE GITMEK ISTIYORSANIZ (Y) BASIN -----------: " )
+    if(q == "Y"):
+	os.system("clear")
+	os.system("figlet Sytem Info Tool")
+	print(M)
+
+else:
+	print("Hatalı Seçim Yaptınız. Program Kapatılıyor......")
